4d59da9eff4d945d434d29a1fc0ad53554fba691
= <90lineng@gmail.com>
2022-07-26 18:54:10 -0400

    "Move Class created"

---
+++ pokemon-project/src/Move.java
@@ -0,0 +1,30 @@
+
+public class Move {
+	private String type;
+	private Integer attack;
+	
+	public Move() {
+
+	}
+	
+	public String getType() {
+		return type;
+	}
+	
+	public Integer getAttack() {
+		return attack;
+	}
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public void setAttack(Integer attack) {
+		this.attack = attack;
+	}
+
+	@Override
+	public String toString() {
+		return "Move [type=" + type + ", attack=" + attack + "]";
+	}
+}
