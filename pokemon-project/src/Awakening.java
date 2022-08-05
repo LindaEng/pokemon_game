3d4ea8a3be81431cdffe26a1ee448a641b8c7dd1
Elvis <elvisgarcia93@gmail.com>
2022-08-05 16:07:10 -0400

    Add isSleeping property to Pokemon. Created an Item to awaken pokemon that are asleep

---
+++ pokemon-project/src/Awakening.java
@@ -0,0 +1,25 @@
+
+public class Awakening extends Item {
+	public Awakening() {
+		super();
+		this.description = "Removes Sleep Status from your Pokemon";
+				
+	}
+	
+	@Override
+	public String getDescription() {
+		return super.getDescription();
+	}
+	
+	@Override
+	public void useItem(Pokemon pokemon) {
+		if(pokemon.getIsSleeping()) {
+			pokemon.setIsSleeping(false);
+		}else {
+			throw new Error("Can only use on pokemon that are asleep");
+		}
+	}
+	
+	
+	//Please add the To String Method for this class on your end (I don't have the data I need here)
+}
